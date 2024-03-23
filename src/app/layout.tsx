6ffb3fa25954158673befcafdf79c074dd626334
Khalil Bohner <khalilbohner@gmail.com>
2024-03-23 14:08:57 -0300

    🍁 Pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Nunito } from 'next/font/google';
 import { ReactNode } from 'react';
 import './globals.css';
 import { ToastProvider } from '@/components/global/toast/ToastProvider';
+import Script from 'next/script';
 
 const nunito = Nunito({
 	subsets: ['latin'],
@@ -31,6 +32,29 @@ export default function RootLayout({ children }: IRootLayoutProps) {
 				<AuthContextProvider>
 					<ToastProvider>{children}</ToastProvider>
 				</AuthContextProvider>
+
+				<Script id="fb-pixel" strategy="afterInteractive">
+					{`
+					!function(f,b,e,v,n,t,s)
+					{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+					n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+					if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+					n.queue=[];t=b.createElement(e);t.async=!0;
+					t.src=v;s=b.getElementsByTagName(e)[0];
+					s.parentNode.insertBefore(t,s)}(window, document,'script',
+					'https://connect.facebook.net/en_US/fbevents.js');
+					fbq('init', '1262126871413263');
+					fbq('track', 'PageView');
+					<noscript>
+						<img
+							height="1"
+							width="1"
+							style="display:none"
+							src="https://www.facebook.com/tr?id=1262126871413263&ev=PageView&noscript=1"
+						/>
+					</noscript>
+					`}
+				</Script>
 			</body>
 		</html>
 	);
